a5ea10d15ad73585b7e891d339a4317de1baa482
JulianCode <hijuliansosa@protonmail.com>
2020-05-07 13:55:03 -0500

    POO 00: Cómo funcionan las clases en JavaScript : Reto

--- poo.js
+++ poo.js
@@ -1,18 +1,23 @@
-function Persona(nombre, apellido) {
+function Persona(nombre, apellido, altura) {
   // Esta función se va a ejecutar cuando creemos una nueva persona
   // Implicitamente retorna this cuando llamamos esta función con la palabra 'new'
   this.nombre = nombre
   this.apellido = apellido
+  this.altura = altura
 }
 
 Persona.prototype.saludar = function() {
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
+Persona.prototype.soyAlta = function() {
+  return this.altura >= 1.80;
+}
 
-var julian = new Persona('Julian', 'Sosa')
-var carlos = new Persona('Carlos', 'Sosa')
-var valentina = new Persona('Valentina', 'Sosa')
-var ginna = new Persona('Ginna', 'Mora')
+var julian = new Persona('Julian', 'Sosa', '1.80')
+var carlos = new Persona('Carlos', 'Sosa', '1.82')
+var valentina = new Persona('Valentina', 'Sosa', '1.22')
+var ginna = new Persona('Ginna', 'Mora', '1.67')
+var pomodoro = new Persona('Pomodoro', 'Time', '1.75')
 
 julian.saludar()
 carlos.saludar()
@@ -27,7 +32,7 @@ ginna.saludar()
 
   - expresiones
   - declaraciones
-  
+
   Reto: agrega el atributo altura y la función soyAlto.
 
  */
