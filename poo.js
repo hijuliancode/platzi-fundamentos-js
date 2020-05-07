749e53c057dcfe1d579b3bf928a002d5ff9bbb42
JulianCode <hijuliansosa@protonmail.com>
2020-05-07 16:19:09 -0500

    POO 04: Clases en JavaScript

--- poo.js
+++ poo.js
@@ -1,37 +1,25 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
-  // Lo que haremos aquí es decirle al prototipo hijo, quien será su prototipo padre
-  // Básicmente asignaremos un prototipo al prototipo/'clase' hija (prototipoHijo)
-  var fn = function () {}
-  fn.prototype = prototipoPadre.prototype;
-  // A continuación le decimos al prototipo hijo que su prototype va a ser un nuevo objeto de fn
-  prototipoHijo.prototype = new fn 
-  // Además lo otro que tenemos que hacer, es asignar la función constructora
-  prototipoHijo.prototype.constructor = prototipoHijo
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+  }
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+  }
+  soyAlta() {
+    console.log(this.altura >= 1.80);
+    return this.altura >= 1.80;
+  }
 }
 
-function Persona(nombre, apellido, altura) {
-  this.nombre = nombre
-  this.apellido = apellido
-  this.altura = altura
-}
-
-Persona.prototype.saludar = function() {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-}
-Persona.prototype.soyAlta = function() {
-  console.log(this.altura >= 1.80);
-  return this.altura >= 1.80;
-}
-
-function Desarrollador(nombre, apellido) {
-  this.nombre = nombre;
-  this.apellido = apellido;
-}
-
-heredaDe(Desarrollador, Persona);
-
-Desarrollador.prototype.saludar = function() {
-  console.log(`Hola me llamo ${this.nombre} ${(this.apellido).charAt(0)} y soy desarrollador/a`)
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura) {
+    super(nombre, apellido, altura); // De esta manera llamamos al constructor de la clase padre
+  }
+  saludar() {
+    console.log(`Hola me llamo ${this.nombre} ${(this.apellido).charAt(0)} y soy desarrollador/a`)
+  }
 }
 
 var julian = new Persona('Julian', 'Sosa', 1.80)
@@ -45,12 +33,13 @@ carlos.soyAlta()
 valentina.soyAlta()
 ginna.soyAlta()
 
-// La verdad oculta sobre las clases en JavaScript
+// Clases en JavaScript
 /**
-  Los objetos en JavaScript son “contenedores” dinámicos de propiedades.
-  Estos objetos poseen un enlace a un objeto prototipo.
-  Cuando intentamos acceder a la propiedad de un objeto,
-  la propiedad no sólo se busca en el propio objeto sino también en el prototipo del objeto,
-  en el prototipo del prototipo, y así sucesivamente hasta que se encuentre una propiedad
-  que coincida con el nombre o se alcance el final de la cadena de prototipos.
+  Las clases de JavaScript son introducidas en el ECMAScript 2015 y 
+  son una mejora en la sintaxis sobre la herencia basada en prototipos de JavaScript.
+
+  La palabra clave extends se usa en declaraciones de clase o expresiones de clasepara crear una clase que es hija de otra clase.
+
+  El método constructor es un método especial para crear e inicializar un objeto creado a partir de una clase.
+
  */
