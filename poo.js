69a8e7b375c244609579524a637c4b32e181930e
JulianCode <hijuliansosa@protonmail.com>
2020-05-07 15:25:12 -0500

    POO 03: La verdad oculta sobre las clases en JavaScript

--- poo.js
+++ poo.js
@@ -1,7 +1,15 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  // Lo que haremos aquí es decirle al prototipo hijo, quien será su prototipo padre
+  // Básicmente asignaremos un prototipo al prototipo/'clase' hija (prototipoHijo)
+  var fn = function () {}
+  fn.prototype = prototipoPadre.prototype;
+  // A continuación le decimos al prototipo hijo que su prototype va a ser un nuevo objeto de fn
+  prototipoHijo.prototype = new fn 
+  // Además lo otro que tenemos que hacer, es asignar la función constructora
+  prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
-  // Buena practiva definir los prototipos al inicio del documento para que siempre esten accesibles hacia todos los objetos que vayamos creando
-  // Esta función se va a ejecutar cuando creemos una nueva persona
-  // Implicitamente retorna this cuando llamamos esta función con la palabra 'new'
   this.nombre = nombre
   this.apellido = apellido
   this.altura = altura
@@ -14,26 +22,35 @@ Persona.prototype.soyAlta = function() {
   console.log(this.altura >= 1.80);
   return this.altura >= 1.80;
 }
-// No se puede ejecutar la anterior función con () => debido a que
-// This en arrow function no hace referencia a su entorno sino al superior de donde su función ha sido definida,
-// En eesta caso windows, this===window = true
-// Es mejor si este prototipo no usa arrow function sino function
 
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+}
+
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function() {
+  console.log(`Hola me llamo ${this.nombre} ${(this.apellido).charAt(0)} y soy desarrollador/a`)
+}
 
 var julian = new Persona('Julian', 'Sosa', 1.80)
 var carlos = new Persona('Carlos', 'Sosa', 1.82)
 var valentina = new Persona('Valentina', 'Sosa', 1.22)
-var ginna = new Persona('Ginna', 'Mora', 1.67)
-var pomodoro = new Persona('Pomodoro', 'Time', 1.75)
+var ginna = new Desarrollador('Ginna', 'Mora', 1.67)
+var pomodoro = new Desarrollador('Pomodoro', 'Time', 1.75)
 
 julian.soyAlta()
 carlos.soyAlta()
 valentina.soyAlta()
 ginna.soyAlta()
 
-// El contexto de las funciones: quién es this
+// La verdad oculta sobre las clases en JavaScript
 /**
-  En esta clase explicamos por qué al introducir el arrow function salió un error. El error del contexto de this en javascrip es uno de los errores más comunes.
-  Recuerda que dentro de la arrow function, this está haciendo referencia al espacio global, a windows.
-
+  Los objetos en JavaScript son “contenedores” dinámicos de propiedades.
+  Estos objetos poseen un enlace a un objeto prototipo.
+  Cuando intentamos acceder a la propiedad de un objeto,
+  la propiedad no sólo se busca en el propio objeto sino también en el prototipo del objeto,
+  en el prototipo del prototipo, y así sucesivamente hasta que se encuentre una propiedad
+  que coincida con el nombre o se alcance el final de la cadena de prototipos.
  */
