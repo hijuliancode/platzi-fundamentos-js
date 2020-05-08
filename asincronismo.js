17b39e3c9735e3001d021df92bd49d49618de07e
JulianCode <hijuliansosa@protonmail.com>
2020-05-07 20:05:39 -0500

    ASINCRONISMO 03: Haciendo múltiples requests

--- asincronismo.js
+++ asincronismo.js
@@ -1,24 +1,26 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id';
-
-const idPersona = prompt('A quien quieres llamar?, Dígita un número mayor a 1');
-
-const personaUrl = `${API_URL}${PEOPLE_URL.replace(':id', idPersona)}`;
 const opts = { crossDomain: true }
 
+// const idPersona = prompt('A quien quieres llamar?, Dígita un número mayor a 1');
+
 const onPeopleResponse = function (persona, status) { // Esta función solo se va a ejecutar si el request es exitoso y la va a ejecutar $.get
-  console.log(arguments); // arguments Es una variable que tienen todas las funciones que nos da un array con los parametros que recibe la función
+  // console.log(arguments); // arguments Es una variable que tienen todas las funciones que nos da un array con los parametros que recibe la función
   console.log(`Hola yo soy ${persona.name}`)
 }
 
-$.get(personaUrl, opts, onPeopleResponse);
+function obtenerPersonaje(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+  $.get(url, opts, onPeopleResponse);
+}
 
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
+obtenerPersonaje(4)
+obtenerPersonaje(5)
 
-// Dígita
+// Callbacks
 /**
-  En esta clase aprenderemos que son los callbacks y usaremos una librería externa que se llama jQuery.
-
-  Un callback es una función que se pasa a otra función como un argumento.
-  Esta función se invoca, después, dentro de la función externa para completar alguna acción.
-  
+  En esta clase accederemos a múltiples datos al mismo tiempo. Continuaremos trabajando con los jQuery y swapi  
  */
