ae81eb8d428b616e1865d6508f32a2e54d2015a3
JulianCode <hijuliansosa@protonmail.com>
2020-05-07 21:20:52 -0500

    ASINCRONISMO 04: Manejando el Orden y el Asincronismo en JavaScript

--- asincronismo.js
+++ asincronismo.js
@@ -2,25 +2,41 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id';
 const opts = { crossDomain: true }
 
-// const idPersona = prompt('A quien quieres llamar?, Dígita un número mayor a 1');
-
-const onPeopleResponse = function (persona, status) { // Esta función solo se va a ejecutar si el request es exitoso y la va a ejecutar $.get
-  // console.log(arguments); // arguments Es una variable que tienen todas las funciones que nos da un array con los parametros que recibe la función
-  console.log(`Hola yo soy ${persona.name}`)
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
-  $.get(url, opts, onPeopleResponse);
+
+  $.get(url, opts, function (persona) {
+    console.log(`Hola yo soy ${persona.name}`)
+    if (callback) {
+      callback()
+    }
+  });
+
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
-obtenerPersonaje(4)
-obtenerPersonaje(5)
+// obtenerPersonaje(1, obtenerPersonaje(2)) // Al colocarlo así, el callback se va a ejecutar inmediatamente, incluso antes de obtener el personaje 1
+// Es importante que la función que se vaya a llamar, no se este ejecutando, que sea el declarar o la referencía a una función pero no su ejecución
 
-// Callbacks
+// CALL BACK HELL
+obtenerPersonaje(1, function() {
+  obtenerPersonaje(2, function() {
+    obtenerPersonaje(3, function() {
+      obtenerPersonaje(4, function() {
+        obtenerPersonaje(5, function() {
+          obtenerPersonaje(6, function() {
+            obtenerPersonaje(7)
+          })
+        })
+      })
+    })
+  })
+})
+
+// Manejando el Orden y el Asincronismo en JavaScript
 /**
-  En esta clase accederemos a múltiples datos al mismo tiempo. Continuaremos trabajando con los jQuery y swapi  
+  Una manera de asegurar que se respete la secuencia en que hemos
+  realizado múltiples tareas es utilizando callbacks, con lo que se ejecutará luego, en cada llamada.
+  
+  Lo importante es que el llamado al callback se haga a través de una función anónima.
+  Sin embargo, al hacerlo de esta manera generamos una situación poco deseada llamada CallbackHell.
  */
