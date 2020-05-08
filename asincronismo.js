5d801d40d0837e59daa965fb1dd6402e8a129aef
JulianCode <hijuliansosa@protonmail.com>
2020-05-08 00:06:25 -0500

    ASINCRONISMO 09: Async-await: lo último en asincronismo

--- asincronismo.js
+++ asincronismo.js
@@ -19,51 +19,23 @@ function onError(id) {
   console.log(`Error al obtener la información del personaje ${id}`)
 }
 
-var ids = [1, 2, 3, 4, 5, 6, 7];
-// var promesas = ids.map((id) => {
-//   return obtenerPersonaje(id);
-// })
-var promesas = ids.map(id => obtenerPersonaje(id))
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7];
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  try {
+    var personajes = await Promise.all(promesas)
+    console.log(personajes)
+  } catch(id) {
+    onError(id)
+  }
+}
 
-Promise
-  .all(promesas)
-  .then(personajes => console.log(personajes))
-  .catch(onError)
+obtenerPersonajes()
 
-// obtenerPersonaje(1) 
-//   .then(personaje1 => {
-//     console.log(`Hola yo soy ${personaje1.name}`)
-//     return obtenerPersonaje(2) // Para devolver una nueva promesa,
-//   })
-//   .then(personaje2 => { // de esta manera .then va a estar devolviendo una promesa ya que el return (linea 25) nos devuelve una promesa
-//     console.log(`Hola yo soy ${personaje2.name}`)
-//     return obtenerPersonaje(3)
-//   })
-//   .then(personaje3 => {
-//     console.log(`Hola yo soy ${personaje3.name}`)
-//     return obtenerPersonaje(4)
-//   })
-//   .then(personaje4 => {
-//     console.log(`Hola yo soy ${personaje4.name}`)
-//     return obtenerPersonaje(5)
-//   })
-//   .then(personaje5 => {
-//     console.log(`Hola yo soy ${personaje5.name}`)
-//     return obtenerPersonaje(6)
-//   })
-//   .then(personaje6 => {
-//     console.log(`Hola yo soy ${personaje6.name}`)
-//     return obtenerPersonaje(7)
-//   })
-//   .then(personaje7 => {
-//     console.log(`Hola yo soy ${personaje7.name}`)
-//   })
-//   .catch(onError)
-
-// Múltiples promesas en paralelo
+// Async-await: lo último en asincronismo
 /**
-  Para hacer el llamado a múltiples promesas,
-  nos apoyamos en un array de ids con el que luego construimos otro arreglo de Promesas,
-  que pasaremos como parámetro a Promise.all( arregloDePromesas ),
-  con las promesas podemos encadenar llamadas en paralelo, algo que no es posible usando callbacks.
+  Async-await es la manera más simple y clara de realizar tareas asíncronas.
+  Await detiene la ejecución del programa hasta que todas las promesas sean resueltas.
+  Para poder utilizar esta forma, hay que colocar async antes de la definición de la función,
+  y encerrar el llamado a Promises.all() dentro de un bloque try … catch.
  */
