70ec6e680f59e15209a3480b8530c82da41cb256
JulianCode <hijuliansosa@protonmail.com>
2020-05-12 23:23:30 -0500

    02 HTML Game, Lighting up the color sequence

--- game/html-game.js
+++ game/html-game.js
@@ -12,11 +12,13 @@ class Game {
 
     this.initialize()
     this.generateSequence()
+    this.nextLevel()
   }
 
   initialize () {
+    console.log('initialize')
     btnStart.setAttribute('disabled', true)
-    this.level = 1
+    this.level = 3
     this.colors = {
       colorBlue,
       colorGreen,
@@ -27,6 +29,73 @@ class Game {
   generateSequence() {
     // Indicar que me genere un nuevo array, .fill inicializa todos los campos en 0
     this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    console.log('this.sequence', this.sequence)
+  }
+  nextLevel() {
+    this.illuminateSequence()
+  }
+  illuminateSequence() {
+    console.log('this.level', this.level)
+    for (let i = 0; i < this.level; i++) {
+      const color = this.numberToColor(this.sequence[i])
+      console.log('illuminateSequence color', color)
+      setTimeout(() => this.illuminateColor(color), 1000 * i);
+    }
+  }
+  numberToColor(num) {
+    console.log('numberToColor', num)
+    switch (num) {
+      case 0:
+        return 'colorBlue'
+      case 1:
+        return 'colorGreen'
+      case 2:
+        return 'colorRed'
+      case 3:
+        return 'colorYellow'
+    }
+  }
+  illuminateColor(color) {
+    console.log('illuminateColor', color)
+    switch (color) {
+      case 'colorBlue':
+        this.colors[color].classList.remove('btn-outline-primary')
+        this.colors[color].classList.add('btn-primary')
+        break;
+      case 'colorGreen':
+        this.colors[color].classList.remove('btn-outline-success')
+        this.colors[color].classList.add('btn-success')
+        break;
+      case 'colorRed':
+        this.colors[color].classList.remove('btn-outline-danger')
+        this.colors[color].classList.add('btn-danger')
+        break;
+      case 'colorYellow':
+        this.colors[color].classList.remove('btn-outline-warn')
+        this.colors[color].classList.add('btn-warn')
+        break;
+    }
+    setTimeout(() => this.turnOffColor(color), 350);
+  }
+  turnOffColor(color) {
+    switch (color) {
+      case 'colorBlue':
+        this.colors[color].classList.remove('btn-primary')
+        this.colors[color].classList.add('btn-outline-primary')
+        break;
+      case 'colorGreen':
+        this.colors[color].classList.remove('btn-success')
+        this.colors[color].classList.add('btn-outline-success')
+        break;
+      case 'colorRed':
+        this.colors[color].classList.remove('btn-danger')
+        this.colors[color].classList.add('btn-outline-danger')
+        break;
+      case 'colorYellow':
+        this.colors[color].classList.remove('btn-warn')
+        this.colors[color].classList.add('btn-outline-warn')
+        break;
+    }
   }
 }
 
